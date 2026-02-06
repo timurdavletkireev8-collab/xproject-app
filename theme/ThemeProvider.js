fc53a186d286e9e165ac86bd84bf00b138377bfb
xixixi <timurdavletkireev8@gmail.com>
2026-02-06 11:36:52 +0400

    Create ThemeProvider.js

---
+++ theme/ThemeProvider.js
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = () => setIsDark(!isDark);
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
